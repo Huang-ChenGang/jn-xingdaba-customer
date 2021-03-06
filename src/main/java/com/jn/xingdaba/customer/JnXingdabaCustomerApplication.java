fc4bcc48ffcc4116af57a0ea2ae64aa907cdef3b
Huang ChenGang <hcggang@163.com>
2021-03-06 16:08:57 +0800

    init project

---
+++ src/main/java/com/jn/xingdaba/customer/JnXingdabaCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnXingdabaCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnXingdabaCustomerApplication.class, args);
+    }
+
+}
