ef5db44b1a93dbe8034d36be4335e213d6c285a8
Huang ChenGang <hcggang@163.com>
2021-04-08 22:15:57 +0800

    add coupon definition save check

--- xingdaba-customer-application/src/main/java/com/jn/xingdaba/customer/api/CouponDefineSaveRequestData.java
+++ xingdaba-customer-application/src/main/java/com/jn/xingdaba/customer/api/CouponDefineSaveRequestData.java
@@ -3,6 +3,8 @@ package com.jn.xingdaba.customer.api;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -11,14 +13,19 @@ public final class CouponDefineSaveRequestData {
 
     private String id;
 
+    @NotBlank(message = "主标题不能为空")
     private String mainTitle;
 
+    @NotBlank(message = "副标题不能为空")
     private String subTitle;
 
+    @NotNull(message = "有效金额不能为空")
     private BigDecimal valueAmount;
 
+    @NotNull(message = "条件金额不能为空")
     private BigDecimal conditionAmount;
 
+    @NotBlank(message = "验证类型不能为空")
     private String validType;
 
     private Integer validDay;
@@ -29,6 +36,7 @@ public final class CouponDefineSaveRequestData {
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDateTime validDateEnd;
 
+    @NotBlank(message = "赠送类型不能为空")
     private String giveType;
 
     private String isDelete;
